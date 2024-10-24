c531ec6ab972065c22594e8d73882c2f7ba34800
yunajoe <yunajoe@gmail.com>
2024-10-24 16:10:19 +0900

    fronted repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+// https://study-ihl.tistory.com/89
+// https://www.youtube.com/watch?v=LP9Y35D4x_8
+// https://graphql.org/blog/2016-05-02-rest-api-graphql-wrapper/#installation
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>하이</h1>
+    </>
+  );
+}
+
+export default App;
